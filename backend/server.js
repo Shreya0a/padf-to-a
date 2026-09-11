b47672324c4faf16ef321a168a0eaf32f316e461
Shreya0a <sharmashreya69774@gmail.com>
2026-09-11 22:47:22 +0530

    Shreya

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const start = async () => {
   }
 
   const port = process.env.PORT || 5000;
-  app.listen(port, () => console.log(`Server running on port ${port}`));
+  app.listen(port, () => console.log(`Server running on port ${port}`)) ;
 };
 
 start();
